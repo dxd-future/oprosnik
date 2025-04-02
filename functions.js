233e0c22af42081ec63dbf31b750eb103a52debd
dxd-future <obuhovsemen797@gmail.com>
2025-04-02 19:23:52 +0900

    0.2

--- functions.js
+++ functions.js
@@ -1,5 +1,56 @@
+let n = 0;
 let start = () => {
     document.querySelector('.start_panel').style.display = 'none';
     document.querySelector('.questions_panel').style.display = 'flex';
-    
+
+    document.querySelector('.question_text').textContent = `${questions[0].question}`;
+    document.querySelector('.question_num').textContent = `${n+1}`;
+    for(let i = 0; i < 4; i++){
+        document.querySelector('.radio_answers').innerHTML += `
+        <p class="answer"> <input class="answer_radio" type="radio" name="answer_radio" >${questions[0].options[i]}</input></p>
+    `;
+    }
+}
+
+let back = () => {
+    if(n > 0){
+        n--;
+        document.querySelector('.question_text').textContent = `${questions[n].question}`;
+        document.querySelector('.question_num').textContent = `${n+1}`;
+    } 
+    if(n == 0){
+        document.querySelector(".radio_answers").style.display = `flex`;
+    }
+    if(questions[n].options.length != 0){
+        for(let i = 0; i < 4; i++){
+            document.querySelector('.radio_answers').innerHTML += `
+            <p class="answer"> <input class="answer_radio" type="radio" name="answer_radio" >${questions[n].options[i]}</input></p>
+        `;
+        }
+    }
+}
+let next = () => {
+    if(n < questions.length - 1){
+        n++;
+        document.querySelector('.question_text').textContent = `${questions[n].question}`;
+        document.querySelector('.question_num').textContent = `${n+1}`;
+    } 
+    if(n != 0){
+        document.querySelector(".radio_answers").style.display = `none`;
+    }
+    if(questions[n].options.length != 0){
+        for(let i = 0; i < 4; i++){
+            document.querySelector('.radio_answers').innerHTML += `
+            <p class="answer"> <input class="answer_radio" type="radio" name="answer_radio" >${questions[n].options[i]}</input></p>
+        `;
+        }
+    }
+}
+
+let toanswer = () => {
+
+}
+
+let end = () => {
+
 }
--- main.js
+++ main.js
@@ -2,65 +2,67 @@ let questions = [
     {
         id: 1,
         question: "Какие из этих городов являются столицами?",
-        options:{
-            op1: "Берлин",
-            op2: "Сидней",
-            op3: "Торонто",
-            op4: "Рио-де-Жанейро"
-        },
+        options:["Берлин","Сидней","Торонто","Рио-де-Жанейро"],
         answer: 1,
     },
     {
         id: 2,
         question: "Какой элемент имеет атомный номер 1?",
-        options:{
-            op1: "Гелий",
-            op2: "Водород",
-            op3: "Кислород",
-            op4: "Углерод"
-        },
+        options:["Гелий","Водород","Кислород","Углерод"],
         answer: 2,
     },
     {
         id: 3,
         question: "Чему равно число π с точностью до двух знаков после запятой?",
+        options:[],
         answer: "3,14",
     },
     {
         id: 4,
         question: "Какие произведения написал Лев Толстой?",
-        answer: "",
+        options:["Война и мир","Преступление и наказание","Анна Каренина","Отцы и дети"],
+        answer: "13"
     },
     {
         id: 5,
-        question: "",
-        answer: "",
+        question: "Сколько хромосом у здорового человека?",
+        options:["44","46","48","50"],
+        answer: "2"
     },
     {
         id: 6,
-        question: "",
-        answer: "",
+        question: "В каком году началась Вторая мировая война?",
+        options:[],
+        answer: "1939"
     },
     {
         id: 7,
-        question: "",
-        answer: "",
+        question: "Какие планеты относятся к газовым гигантам?",
+        options:["Марс","Юпитер","Венера","Сатурн"],
+        answer: "24"
     },
     {
         id: 8,
-        question: "",
-        answer: "",
+        question: "Какая единица измерения силы?",
+        options:["Паскаль","Ньютон","Ватт","Джоуль"],
+        answer: "2"
     },
     {
         id: 9,
-        question: "",
-        answer: "",
+        question: "Какой язык создал Гвидо ван Россум?",
+        options:[],
+        answer: "Python"
     },
     {
         id: 10,
-        question: "",
-        answer: "",
+        question: "Назовите столицу Японии.",
+        options:[],
+        answer: "Токио"
     }
 ]
 
 document.querySelector('#start_button').addEventListener('click', start);
+document.querySelector('#back_button').addEventListener('click', back);
+document.querySelector('#next_button').addEventListener('click', next);
+document.querySelector('#toanswer_button').addEventListener('click', toanswer);
+document.querySelector('#end_button').addEventListener('click', end);
